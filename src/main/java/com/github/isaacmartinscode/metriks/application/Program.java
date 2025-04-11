f3959986173bcf76164739a30b6295e09c0ec71c
Isaac Martins <isaacmartins.code@gmail.com>
2025-04-11 18:43:02 -0300

    feat: Classe Program modificada

--- src/main/java/com/github/isaacmartinscode/metriks/application/Program.java
+++ src/main/java/com/github/isaacmartinscode/metriks/application/Program.java
@@ -1,16 +1,29 @@
 package com.github.isaacmartinscode.metriks.application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.io.IOException;
 
 public class Program extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+    private static Scene mainScene;
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Program.class.getResource("CpuView.fxml"));
+        mainScene = new Scene(fxmlLoader.load(), 950, 550);
+        stage.setResizable(false);
+        stage.setScene(mainScene);
+        stage.show();
+    }
 
+    public static Scene getMainScene() {
+        return mainScene;
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
