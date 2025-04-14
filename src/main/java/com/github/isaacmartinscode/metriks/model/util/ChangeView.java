f51d8e29011be5813d4d2d8ea184ff7dbd8bfb3c
Isaac Martins <isaacmartins.code@gmail.com>
2025-04-14 15:07:55 -0300

    add: Classe utilitaria ChangeView adicionada

---
+++ src/main/java/com/github/isaacmartinscode/metriks/model/util/ChangeView.java
@@ -0,0 +1,25 @@
+package com.github.isaacmartinscode.metriks.model.util;
+
+import com.github.isaacmartinscode.metriks.application.Program;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+public class ChangeView {
+    public synchronized static void change(String absoluteName) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Program.class.getResource(absoluteName));
+            AnchorPane newAnchorPane = fxmlLoader.load();
+
+            Scene mainScene = Program.getMainScene();
+            AnchorPane mainAP = (AnchorPane) mainScene.getRoot();
+            mainAP.getChildren().clear();
+            mainAP.getChildren().addAll(newAnchorPane);
+        }
+        catch(IOException e) {
+           System.out.print("Error: " + e.getMessage());
+        }
+    }
+}
