e478216e3f948a6c1a860b475be081a92db8d78c
Isaac Martins <isaacmartins.code@gmail.com>
2025-04-15 16:00:14 -0300

    feat: Botões de navegação agora funcionam para navegar entre Cpu e MemView (método que chama classe ChangeView adicionado aos controllers)

--- src/main/java/com/github/isaacmartinscode/metriks/controller/CpuViewController.java
+++ src/main/java/com/github/isaacmartinscode/metriks/controller/CpuViewController.java
@@ -1,4 +1,10 @@
 package com.github.isaacmartinscode.metriks.controller;
 
+import com.github.isaacmartinscode.metriks.model.util.ChangeView;
+
 public class CpuViewController {
+
+    public void onMemButtonAction() {
+        ChangeView.change("/com/github/isaacmartinscode/metriks/views/MemView.fxml");
+    }
 }
--- src/main/java/com/github/isaacmartinscode/metriks/controller/MemViewController.java
+++ src/main/java/com/github/isaacmartinscode/metriks/controller/MemViewController.java
@@ -1,4 +1,10 @@
 package com.github.isaacmartinscode.metriks.controller;
 
+import com.github.isaacmartinscode.metriks.model.util.ChangeView;
+
 public class MemViewController {
+
+    public void onCpuButtonAction() {
+        ChangeView.change("/com/github/isaacmartinscode/metriks/views/CpuView.fxml");
+    }
 }
