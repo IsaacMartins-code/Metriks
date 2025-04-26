f4b0281ab2fa73c6880c8a4356c3d6c3a8ed00cd
Isaac Martins <isaacmartins.code@gmail.com>
2025-04-26 15:47:26 -0300

    build: OSHI configurado no module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.github.isaacmartinscode.metriks {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.github.oshi;
 
     opens com.github.isaacmartinscode.metriks.controller to javafx.fxml;
     opens com.github.isaacmartinscode.metriks.views to javafx.fxml;
