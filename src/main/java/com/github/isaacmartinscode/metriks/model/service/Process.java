98997587a386a5be7bb12d856ca68f1efe5960e5
Isaac Martins <isaacmartins.code@gmail.com>
2025-04-26 21:29:34 -0300

    att: Superclasse Process implementada

--- src/main/java/com/github/isaacmartinscode/metriks/model/service/Process.java
+++ src/main/java/com/github/isaacmartinscode/metriks/model/service/Process.java
@@ -1,4 +1,50 @@
 package com.github.isaacmartinscode.metriks.model.service;
 
 public abstract class Process {
+
+    private String name;
+    private int pId;
+    private int threads;
+    private String user;
+
+    public Process(String name, int pId, int threads, String user) {
+        this.name = name;
+        this.pId = pId;
+        this.threads = threads;
+        this.user = user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getpId() {
+        return pId;
+    }
+
+    public void setpId(int pId) {
+        this.pId = pId;
+    }
+
+    public int getThreads() {
+        return threads;
+    }
+
+    public void setThreads(int threads) {
+        this.threads = threads;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public abstract double calcUsagePercentage();
 }
