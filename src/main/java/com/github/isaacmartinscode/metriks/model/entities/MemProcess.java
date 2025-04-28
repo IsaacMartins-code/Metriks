cf2f57ca2631eb1e580e80b1f3d5e5f07bf463d2
Isaac Martins <isaacmartins.code@gmail.com>
2025-04-28 19:03:58 -0300

    att: Subclasse MemProcess implementada

--- src/main/java/com/github/isaacmartinscode/metriks/model/entities/MemProcess.java
+++ src/main/java/com/github/isaacmartinscode/metriks/model/entities/MemProcess.java
@@ -1,4 +1,19 @@
 package com.github.isaacmartinscode.metriks.model.entities;
 
 public class MemProcess extends Process {
+
+    private double memUsagePercentage;
+
+    public MemProcess(String name, int pId, int threads, String user) {
+        super(name, pId, threads, user);
+    }
+
+    public double getMemUsagePercentage() {
+        return memUsagePercentage;
+    }
+
+    public void calcUsagePercentage(long memBytesUsage, double totalMemory) {
+        double memGbUsage = memBytesUsage / Math.pow(1024, 3);
+        memUsagePercentage = memGbUsage * 100.0 / totalMemory;
+    }
 }
